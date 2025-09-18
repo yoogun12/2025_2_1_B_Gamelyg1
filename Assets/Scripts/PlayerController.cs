8d0e7207c66f596b26081a533bcfa2e26003e560
yoogun12 <yoogun1313@gmail.com>
2025-09-18 11:39:21 +0900

    [Feat] 애니메이션 점프 , 공격 , GameManager 제작

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,11 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 3.0f;
     public float runSpeed = 6.0f;
     public float rotationSpeed = 10.0f;
+
+    [Header("점프 설정")]
+    public float jumpHeight = 2.0f;
+    public float gravity = -9.81f;                                  //중력 속도 추가
+    public float landingDuration = 3.0f;                            //착지 후 착지 모션 지속 시간 ( 해당 지속 시간 이후에 캐릭터가 움직일 수 있게)
 
     [Header("공격 설정")]
     public float attackDuration = 0.8f;                             //공격 지속 시간
@@ -22,6 +28,13 @@ public class PlayerController : MonoBehaviour
     //현재 상태 값들
     private float currentSpeed;
     private bool isAttacking = false;
+    private bool isLanding = false;                                 //착지 중인지 확인
+    private float landingTimer;                                     //착지 타이머
+
+    private Vector3 velocity;
+    private bool isGrounded;                                        //땅에 있는지 판별
+    private bool wasGrounded;                                       //직전 프레임에 땅에 있었는지 판단
+    private float attackTimer;                                      
 
     // Start is called before the first frame update
     void Start()
@@ -33,13 +46,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckGrounded();
+        HandleLanding();
         HandleMovement();
         UpdateAnimator();
-
+        HandleAttack();
+        HandleJump();
     }
 
     void HandleMovement()                       //이동 함수 제작
     {
+
+        //공격 중이거나 착지 중일 때 움직임 제한
+        if((isAttacking && !canMoveWhileAttacking) || isLanding)
+        {
+            currentSpeed = 0f;
+            return;
+        }
+
         float horizontal = Input.GetAxis("Horizontal");
         float verical = Input.GetAxis("Vertical");
 
@@ -81,5 +105,89 @@ public class PlayerController : MonoBehaviour
         //전체 최대속도 (runSpeed) 기준으로 0 ~ 1 계산
         float animatorSpeed = Mathf.Clamp01(currentSpeed / runSpeed);
         animator.SetFloat("speed",animatorSpeed);
+        animator.SetBool("isGrounded",isGrounded);
+
+        bool isFalling = !isGrounded && velocity.y < -0.1f;                 //캐릭터가 Y축 속도가 음수로 넘어가면 떨어지고 있다고 판단
+        animator.SetBool("isFalling",isFalling);
+        animator.SetBool("isLanding",isLanding);
     }
+
+    void CheckGrounded()
+    {
+        wasGrounded = isGrounded;
+        isGrounded = controller.isGrounded;                     //캐릭터 컨트롤러에서 상태 값을 받아온다.
+
+        if (!isGrounded && wasGrounded)                         //지금 프레임은 땅이 아니고, 이전 프레임은 땅
+        {
+            Debug.Log("떨어지기 시작");
+        }
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
+
+            if(!wasGrounded && animator != null)                //착지를 진행
+            {
+                isLanding = true;
+                landingTimer = landingDuration;
+            }
+        }
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            if(animator != null)
+            {
+                animator.SetTrigger("jumpTrigger");
+            }
+        }
+
+        if (!isGrounded)                                             //땅위에 있지 않을경우 중력 적용
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleLanding()
+    {
+        if (isLanding)
+        {
+            landingTimer -= Time.deltaTime;                           //랜딩 타이머 시간 만큼 못 움직이게
+
+            if(landingTimer <=0)
+            {
+                isLanding = false;                                    //착지 완료 처리
+            }
+        }
+    }
+
+    void HandleAttack()
+    {
+        if(isAttacking)                                               //공격 중일때
+        {
+            attackTimer -= Time.deltaTime;                            //공격 타이머를 감소
+
+            if(attackTimer <=0)
+            {
+                isAttacking = false;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1) && !isAttacking)            //공격 중이 아닐때 키를 누르면 공격
+        {
+            isAttacking = true;                                         //공격중  표시
+            attackTimer = attackDuration;                               //타이머 리필
+
+            if(animator != null)
+            {
+                animator.SetTrigger("attackTrigger");
+            }
+        }
+    }
+
 }
